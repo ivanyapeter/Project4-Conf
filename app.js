e8afcf025a4d845272f500b6be2ffbcbd0f3d30a
Kylie Sy <kylksy@gmail.com>
2015-12-07 22:13:12 +1100

    fixed sass not compiling

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // sass
 app.use(sass({
@@ -32,6 +32,9 @@ app.use(sass({
     outputStyle: 'compressed'
     // prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/> 
 }));
+
+// this should be used after sass middleware
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
