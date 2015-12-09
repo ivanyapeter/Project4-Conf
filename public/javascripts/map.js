96970e153a949f50222580b36f2c634e4e23fbc8
Aviel Goh <avielgwk@gmail.com>
2015-12-09 17:36:06 +1100

    amend css for schedule timetable

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-	var latlng = new google.maps.LatLng(-37.8185768, 144.9590125);
+	var latlng = new google.maps.LatLng(-37.818806, 144.959046);
 	var mapOptions = {
-	          zoom: 14,
+	          zoom: 16,
 	          scrollwheel: false,
-	          center: latlng,
+	          center: {lat: -37.817863, lng: 144.952563},
 	          styles: [{
 	            stylers:[
-	            { hue: "#00ffe6" },
-	            { saturation: -20 }
+	            { hue: "#00c775" },
+							{ lightness: -70 },
+	            { saturation: 90 }
 	            ]
 	          }]
 	        };
 	var map = new google.maps.Map( $('.map')[0], mapOptions );
 
+	var image = 'images/map-marker-white.png';
 	var marker = new google.maps.Marker({
-		position: latlng
+		position: latlng,
+		icon: image
 	});
 	marker.setMap(map);
 });
-
